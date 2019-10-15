885aef3feb93627f96a068cf6b69c063146592eb
Benjamin Slater <benjamin.ed.slater@gmail.com>
2019-10-14 22:28:03 -0700

    added pause/play button and toggle ability

--- index.js
+++ index.js
@@ -3,30 +3,36 @@ import { Universe, Cell } from "wasm-game-of-life";
 // Import the WebAssembly memory at the top of the file.
 import { memory } from "wasm-game-of-life/wasm_game_of_life_bg";
 
+// Set contants for displaying grid properly.
 const CELL_SIZE = 5; // px
 const GRID_COLOR = "#CCCCCC";
 const DEAD_COLOR = "#FFFFFF";
 const ALIVE_COLOR = "#000000";
 
+// Initialize universe with semi-random live dead cells.
 const universe = Universe.new();
 const width = universe.width();
 const height = universe.height();
 
+// Initalize canvas to display evolving patterns.
 const canvas = document.getElementById("game-of-life-canvas");
 canvas.height = (CELL_SIZE + 1) * height + 1;
 canvas.width = (CELL_SIZE + 1) * width + 1;
 
 const ctx = canvas.getContext('2d');
+let animationId = null;
 
+// Loop that defines changing cells.
 const renderLoop = () => {
   universe.tick();
 
   drawGrid();
   drawCells();
 
-  requestAnimationFrame(renderLoop);
+  animationId = requestAnimationFrame(renderLoop);
 }
 
+// Helper function to draw grid that contains cells.
 const drawGrid = () => {
   ctx.beginPath();
   ctx.strokeStyle = GRID_COLOR;
@@ -46,10 +52,12 @@ const drawGrid = () => {
   ctx.stroke();
 };
 
+// Helper function to get index in ptr array.
 const getIndex = (row, column) => {
   return row * width + column;
 };
 
+// Helper function to draw cells within in grid.
 const drawCells = () => {
   const cellsPtr = universe.cells();
   const cells = new Uint8Array(memory.buffer, cellsPtr, width * height);
@@ -76,6 +84,51 @@ const drawCells = () => {
   ctx.stroke();
 };
 
+// Set of functions to control the push button play/pause.
+const isPaused = () => {
+  return animationId === null;
+};
+
+const playPauseButton = document.getElementById("play-pause");
+
+const play = () => {
+  playPauseButton.textContent = "⏸";
+  renderLoop();
+};
+
+const pause = () => {
+  playPauseButton.textContent = "▶";
+  cancelAnimationFrame(animationId);
+  animationId = null;
+};
+
+playPauseButton.addEventListener("click", event => {
+  if (isPaused()) {
+    play();
+  } else {
+    pause();
+  }
+});
+
+// Click to change cells on canvas.
+canvas.addEventListener("click", event => {
+  const boundingRect = canvas.getBoundingClientRect();
+
+  const scaleX = canvas.width / boundingRect.width;
+  const scaleY = canvas.height / boundingRect.height;
+
+  const canvasLeft = (event.clientX - boundingRect.left) * scaleX;
+  const canvasTop = (event.clientY - boundingRect.top) * scaleY;
+
+  const row = Math.min(Math.floor(canvasTop / (CELL_SIZE + 1)), height - 1);
+  const col = Math.min(Math.floor(canvasLeft / (CELL_SIZE + 1)), width - 1);
+
+  universe.toggle_cell(row, col);
+
+  drawGrid();
+  drawCells();
+});
+
 drawGrid();
 drawCells();
-requestAnimationFrame(renderLoop);
+play();
